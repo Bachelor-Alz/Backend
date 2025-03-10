2186eb5cf5df354c92d731b45980156962d08017
Daniel Rantzau Bech <dbech22@student.aau.dk>
2025-03-10 14:36:17 +0100

    Fixed Relations

---
+++ HealthDevice/Migrations/20250310133517_InitialCreate.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HealthDevice.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    latitude = table.Column<int>(type: "integer", nullable: false),
+                    longitude = table.Column<int>(type: "integer", nullable: false),
+                    altitude = table.Column<int>(type: "integer", nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Elders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    locationsid = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elders", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Elders_Locations_locationsid",
+                        column: x => x.locationsid,
+                        principalTable: "Locations",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FallInfos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    locationid = table.Column<int>(type: "integer", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FallInfos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_FallInfos_Locations_locationid",
+                        column: x => x.locationid,
+                        principalTable: "Locations",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Heartrates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Rate = table.Column<int>(type: "integer", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Elderid = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heartrates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Heartrates_Elders_Elderid",
+                        column: x => x.Elderid,
+                        principalTable: "Elders",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elders_locationsid",
+                table: "Elders",
+                column: "locationsid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FallInfos_locationid",
+                table: "FallInfos",
+                column: "locationid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Heartrates_Elderid",
+                table: "Heartrates",
+                column: "Elderid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FallInfos");
+
+            migrationBuilder.DropTable(
+                name: "Heartrates");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Elders");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
--- HealthDevice/Models/HeartRate.cs
+++ HealthDevice/Models/HeartRate.cs
@@ -5,5 +5,4 @@ public class Heartrate
     public required int Id { get; set; }
     public int Rate { get; set; }
     public DateTime Timestamp { get; set; }
-    public required Elder elder { get; set; }
 }
