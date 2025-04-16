2f498c23d30108f81ba515dfe4f921e2ebe2d3a9
DanielRBech <dbech22@student.aau.dk>
2025-04-16 10:18:12 +0200

    FakeData

--- HealthDevice/Controllers/TestController.cs
+++ HealthDevice/Controllers/TestController.cs
@@ -1,4 +1,6 @@
-﻿using HealthDevice.Services;
+﻿using HealthDevice.DTO;
+using HealthDevice.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HealthDevice.Controllers;
@@ -9,10 +11,12 @@ namespace HealthDevice.Controllers;
 public class TestController : ControllerBase
 {
     private readonly GeoService _geoService;
+    private readonly UserManager<Elder> _elderManager;
     
-    public TestController(GeoService geoService)
+    public TestController(GeoService geoService, UserManager<Elder> elderManager)
     {
         _geoService = geoService;
+        _elderManager = elderManager;
     }
     
     [HttpPost("Address")]
@@ -27,5 +31,35 @@ public class TestController : ControllerBase
     {
         var result = await _geoService.GetCoordinatesFromAddress(street, city, state, country, postalCode, amenity);
         return Ok(result);
+    }
+    [HttpPost("FakeData")]
+    public async Task<ActionResult> GenerateFakeData(Elder elder)
+    {
+        DateTime currentDate = DateTime.Now;
+        int HeartrangeX = 40;
+        int HeartrangeY = 200;
+        double SpO2rangeX = 0.7;
+        double SpO2rangeY = 1;
+        
+        if (elder.MAX30102Data == null)
+        {
+            elder.MAX30102Data = new List<Max30102>();
+        }
+        
+        for (int i = 0; i < 30000; i++)
+        {
+            DateTime timestamp = currentDate.AddMinutes(i);
+            int heartrate = Random.Shared.Next(HeartrangeX, HeartrangeY);
+            double spo2 = Random.Shared.NextDouble() * (SpO2rangeY - SpO2rangeX) + SpO2rangeX;
+            elder.MAX30102Data.Add(new Max30102
+            {
+                Id = i,
+                Heartrate = heartrate,
+                SpO2 = (float)spo2,
+                Timestamp = timestamp
+            });
+        }
+        await _elderManager.UpdateAsync(elder);
+        return Ok("Fake data generated successfully");
     }
 }
