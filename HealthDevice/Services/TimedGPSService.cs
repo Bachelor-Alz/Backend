3e3784088b35beb457c59ba1cd950d2b9304d47c
DanielRBech <dbech22@student.aau.dk>
2025-04-17 12:28:00 +0200

    AutoAssign Elder to arduino

--- HealthDevice/Services/TimedGPSService.cs
+++ HealthDevice/Services/TimedGPSService.cs
@@ -1,4 +1,5 @@
-﻿using HealthDevice.DTO;
+﻿using HealthDevice.Data;
+using HealthDevice.DTO;
 using Microsoft.AspNetCore.Identity;
 
 namespace HealthDevice.Services
@@ -23,10 +24,27 @@ namespace HealthDevice.Services
                 {
                     UserManager<Elder> elderManager = scope.ServiceProvider.GetRequiredService<UserManager<Elder>>();
                     HealthService healthService = scope.ServiceProvider.GetRequiredService<HealthService>();
+                    ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    GeoService geoService = scope.ServiceProvider.GetRequiredService<GeoService>();
                     List<Elder> elders = elderManager.Users.ToList();
-
+                    //Find all gpsData where the address isnt assigned to an elder arduino
+                    List<GPS> gpsData = dbContext.GPSData.Where(g => g.Address != null && elders.All(e => e.Arduino != g.Address)).ToList();
+                    
                     foreach (Elder elder in elders)
                     {
+                        if (elder.Arduino == null)
+                        {
+                            foreach (GPS gp in gpsData)
+                            {
+                                string GpsAddress = await geoService.GetAddressFromCoordinates(gp.Latitude, gp.Longitude);
+                                if (elder is not { latitude: not null, longitude: not null }) continue;
+                                string elderAddress = await geoService.GetAddressFromCoordinates((double)elder.latitude, (double)elder.longitude);
+                                if (GpsAddress != elderAddress) continue;
+                                elder.Arduino = gp.Address;
+                                _logger.LogInformation("Elder {ElderEmail} assigned to Arduino {Arduino}", elder.Email, gp.Address);
+                                
+                            }
+                        }
                         DateTime currentTime = DateTime.Now;
                         
                         Location location = await healthService.GetLocation(currentTime, elder);
