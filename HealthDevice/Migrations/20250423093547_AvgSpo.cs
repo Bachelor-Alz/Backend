35c6e1b311cdbfb620eec08f8f44efbb27aa9280
DanielRBech <dbech22@student.aau.dk>
2025-04-23 11:37:20 +0200

    Migrations fix

--- HealthDevice/Migrations/20250423093547_AvgSpo.cs
+++ HealthDevice/Migrations/20250423093547_AvgSpo.cs
@@ -5,18 +5,24 @@
 namespace HealthDevice.Migrations
 {
     /// <inheritdoc />
-    public partial class AvgSpo2 : Migration
+    public partial class AvgSpo : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.RenameColumn(
+                name: "SpO2",
+                table: "SpO2",
+                newName: "AvgSpO2");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.RenameColumn(
+                name: "AvgSpO2",
+                table: "SpO2",
+                newName: "SpO2");
         }
     }
 }
