f26932e6086650ac62783353b0df8065812e625c
DanielRBech <dbech22@student.aau.dk>
2025-04-28 09:50:51 +0200

    Fix

--- HealthDevice/Program.cs
+++ HealthDevice/Program.cs
@@ -61,6 +61,7 @@ builder.Services.AddScoped<IGeoService, GeoService>();
 builder.Services.AddScoped<IArduinoService, ArduinoService>();
 builder.Services.AddScoped<IAIService, AiService>();
 builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IGetHealthData, GetHealthDataService>();
 
 builder.Services.ConfigureApplicationCookie();
 
