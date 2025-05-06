407fa44cf22b8516e3579a29c363dffc51215bd4
Daniel R Bech <112968595+DanielRBech@users.noreply.github.com>
2025-05-06 22:54:07 +0200

    Removed UTC to Local and TestUser update (#109)
    * Ensure the program doesnt crash when test user already exist
    
    * Ensure that FakeData makes data with timestamps without seconds
    
    * Removed UTC to "LocalTime"
    
    * Better Error Handling

--- HealthDevice/Utils/TestUserConfig.cs
+++ HealthDevice/Utils/TestUserConfig.cs
@@ -30,10 +30,14 @@ public static class TestUserConfig
             longitude = 12.5683,
             Role = Roles.Elder,
         };
-
+        
         // Register the user
-       await userController.Register(elder);
+       var result = await userController.Register(elder);
 
+        if (result is not OkObjectResult)
+        {
+            return;
+        }
         // Resolve Elder repository and set the MAC address
         var elderRepository = scopedServices.GetRequiredService<IRepository<Elder>>();
         var elderEntity = await elderRepository.Query().FirstOrDefaultAsync(e => e.Email == elder.Email);
